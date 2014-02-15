a1df9f03d4a427bab5378ff22c2333b6b9c9d4e1
David John Zawicki <dzawicki@purdue.edu>
2014-02-15 17:44:20 -0500

    updated function header

--- proj1/sorting_main.c
+++ proj1/sorting_main.c
@@ -7,7 +7,7 @@ int Save_File(char * Filename, long * Array, int Size);
 void Shell_Insertion_Sort(long * Array, int Size, double * N_Comp, double * N_Move);
 void Shell_Selection_Sort(long * Array, int Size, double * N_Comp, double * N_Move);
 int Print_Seq(char * Filename, int Size);
-int * Gen_K_Seq(int Size);
+int Gen_K_Seq(int * arr, int Size);
 
 int main (int argc, char ** argv)
 {
