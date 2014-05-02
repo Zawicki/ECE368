214b6dfe1a3705190aef1e159367872f1b3df52e
David John Zawicki <dzawicki@purdue.edu>
2014-05-02 14:04:29 -0400

    Program complete

--- proj5/river.c
+++ proj5/river.c
@@ -1,15 +1,21 @@
 #include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
-#include <math.h>
 
-typedef struct _graph
+typedef struct _node
 {
-	int weight;
-}Graph;
+	int id;
+	int dist;
+}Node;
 
-Graph * * Load_Graph(char * Filename, int * size);
-//void Dijkstra(Graph * *, int * size, int src, int dest);
+typedef struct _heap
+{
+	Node * v;
+}Queue;
+
+int * * Load_Graph(char * Filename, int * size);
+int Dijkstra(int size, int src, int dest, int * * matrix);
+void Downward_heapify(Queue * pq, int i, int n);
+int compHeap(const void * p1, const void * p2);
 
 int main (int argc, char * * argv)
 {
@@ -22,9 +28,9 @@ int main (int argc, char * * argv)
 	char * in_file = argv[1];
 
 	int size;
-	Graph * * g = Load_Graph(in_file, &size);
+	int * * matrix = Load_Graph(in_file, &size);
 
-	printf("\n  Adjacency Matrix\n\n");
+	/*printf("\n  Adjacency Matrix\n\n");
 	printf("     0 1 2 3 4 5 6 7 8 9 A B C D\n");
 	printf("     ---------------------------\n");
 	int i, j;
@@ -33,20 +39,80 @@ int main (int argc, char * * argv)
 		printf("%3d:", i);
 		for (j = 0; j < size; j++)
 		{
-			printf(" %d", g[i][j].weight);
+			printf(" %d", matrix[i][j]);
 		}
 		printf("\n");
 	}
-	printf("\n\n");
+	printf("\n\n");*/
 
+	int answer = Dijkstra(size, 0, size - 1, matrix);
+	printf("%d\n", answer);
+
+	int i;
 	for (i = 0; i < size; i++)
-		free(g[i]);
-	free(g);
+		free(matrix[i]);
+	free(matrix);
 
 	return EXIT_SUCCESS;
 }
 
-Graph * * Load_Graph(char * Filename, int * size)
+int Dijkstra(int size, int src, int dest, int * * matrix)
+{
+	Node * graph = malloc(sizeof(Node) * size);
+	Queue * PQ = malloc(sizeof(Queue) * size);
+
+	int n = size;
+
+	int i;
+	for (i = 0; i < size; i++)
+	{
+		graph[i].id = i;
+		graph[i].dist = 10000;
+		PQ[i].v = &graph[i];
+	}
+	graph[src].dist = 0;
+
+	Node u;
+	while (size > 0)
+	{
+		u = *(PQ[0].v);
+		PQ[0] = PQ[size - 1];
+		size--;
+		qsort(PQ, size, sizeof(Queue), compHeap);
+
+		for (i = 0; i < n; i++)
+		{
+			if (matrix[u.id][i] >= 0)
+			{
+				if (graph[i].dist > (u.dist + matrix[u.id][i]))
+					graph[i].dist = u.dist + matrix[u.id][i];
+			}
+		}
+	}
+
+	int answer = graph[dest].dist;
+
+	free(PQ);
+	free(graph);
+	return answer;
+}
+
+int compHeap(const void * p1, const void * p2)
+{
+	Queue * ptr1 = (Queue *)p1;
+	Queue * ptr2 = (Queue *)p2;
+
+	Queue elem1 = * ptr1;
+	Queue elem2 = * ptr2;
+
+	if (elem1.v -> dist < elem2.v -> dist)
+		return -1;
+	if (elem1.v -> dist == elem2.v -> dist)
+		return 0;
+	return 1;
+}
+
+int * * Load_Graph(char * Filename, int * size)
 {
 	FILE * f = fopen(Filename, "r");
 
@@ -56,16 +122,16 @@ Graph * * Load_Graph(char * Filename, int * size)
 	int n;
 	fscanf(f, "%d", &n);
 	*size = 2*(n-1)*n + 2;
-	Graph * * g = malloc(sizeof(Graph *) * (*size));
+	int * * matrix = malloc(sizeof(int *) * (*size));
 	
 	//initialize all weights to a large number
 	int i, j;
 	for (i = 0; i < (*size); i++)
 	{
-		g[i] = malloc(sizeof(Graph) * (*size));
+		matrix[i] = malloc(sizeof(int) * (*size));
 		for (j = 0; j < (*size); j++)
 		{
-			g[i][j].weight = 9;
+			matrix[i][j] = -1;
 		}
 	}
 
@@ -92,14 +158,14 @@ Graph * * Load_Graph(char * Filename, int * size)
 
 	//find weights from source to first column
 	int ct = 0;
-	g[*size - 1][*size - 1].weight = 0;
-	g[0][0].weight = 0;
+	matrix[*size - 1][*size - 1] = 0;
+	matrix[0][0] = 0;
 	for (i = n; i < (*size) - 1; i += 2*n - 1)
 	{
 		if (planks[ct][0] == 1)
-			g[0][i].weight = 0;
+			matrix[0][i] = 0;
 		else
-			g[0][i].weight = 1;
+			matrix[0][i] = 1;
 		ct++;
 	}
 
@@ -117,18 +183,18 @@ Graph * * Load_Graph(char * Filename, int * size)
 				if (j == i + n - 1)
 				{
 						if (planks[row + 1][col] == 1)
-							g[i][j].weight = 0;
+							matrix[i][j] = 0;
 						else
-							g[i][j].weight = 1;
+							matrix[i][j] = 1;
 				}
 			
 				//down right
 				if (j == i + n)
 				{
 						if (planks[row + 1][col + 1] == 1)
-							g[i][j].weight = 0;
+							matrix[i][j] = 0;
 						else
-							g[i][j].weight = 1;	
+							matrix[i][j] = 1;
 				}
 			}
 
@@ -139,31 +205,31 @@ Graph * * Load_Graph(char * Filename, int * size)
 				if (j == i - n)
 				{
 						if (planks[row - 1][col] == 1)
-							g[i][j].weight = 0;
+							matrix[i][j] = 0;
 						else
-							g[i][j].weight = 1;	
+							matrix[i][j] = 1;
 				}
 
 				//up right
 				if (j == i - n + 1)
 				{
 						if (planks[row - 1][col + 1] == 1)
-							g[i][j].weight = 0;
+							matrix[i][j] = 0;
 						else
-							g[i][j].weight = 1;	
+							matrix[i][j] = 1;
 				}
 			}
 			//first vertical set of planks
 			else if (i == n + (2*n - 1)*row)
 			{
 				if (j == (i - n + 1) || j == (i + n))
-					g[i][j].weight = 1;
+					matrix[i][j] = 1;
 			}
 			//last vertical set of planks
 			else if (i == n + n - 1 + (2*n - 1)*row)
 			{
 				if (j == ((*size) - 1))
-					g[i][j].weight = 1;
+					matrix[i][j] = 1;
 			}
 			//planks not on perimeter
 			else
@@ -174,36 +240,36 @@ Graph * * Load_Graph(char * Filename, int * size)
 					if (j == i - n)
 					{
 						if (planks[row][col] == 1)
-							g[i][j].weight = 0;
+							matrix[i][j] = 0;
 						else
-							g[i][j].weight = 1;	
+							matrix[i][j] = 1;
 					}
 	
 					//up right
 					if (j == i - n + 1)
 					{
 						if (planks[row][col + 1] == 1)
-							g[i][j].weight = 0;
+							matrix[i][j] = 0;
 						else
-							g[i][j].weight = 1;	
+							matrix[i][j] = 1;
 					}
 	
 					//down left
 					if (j == i + n - 1)
 					{
 						if (planks[row + 1][col] == 1)
-							g[i][j].weight = 0;
+							matrix[i][j] = 0;
 						else
-							g[i][j].weight = 1;	
+							matrix[i][j] = 1;
 					}
 				
 					//down right
 					if (j == i + n)
 					{
 						if (planks[row + 1][col + 1] == 1)
-							g[i][j].weight = 0;
+							matrix[i][j] = 0;
 						else
-							g[i][j].weight = 1;	
+							matrix[i][j] = 1;
 					}
 				}
 				else
@@ -211,23 +277,23 @@ Graph * * Load_Graph(char * Filename, int * size)
 					if (j == i - n + 1 || j == i - n || j == i + n - 1 || j == i + n)
 					{
 						if (j != *size - 1)
-							g[i][j].weight = 1;
+							matrix[i][j] = 1;
 					}
 					else if (j == i - 2*n + 1)
 					{
 						if (planks[row - 1][col] == 1)
-							g[i][j].weight = 0;
+							matrix[i][j] = 0;
 					}
 					else if (j == i + 2*n - 1)
 					{
 						if (planks[row + 1][col] == 1)
-							g[i][j].weight = 0;
+							matrix[i][j] = 0;
 					}
 				}
 			}
 
 			if (j == i)
-				g[i][j].weight = 0;
+				matrix[i][j] = 0;
 		}
 		//determine if plank is vertical or horizontal
 		col++;
@@ -235,7 +301,6 @@ Graph * * Load_Graph(char * Filename, int * size)
 		{
 			h = 0;
 			col = 0;
-			printf("row = %d", row);
 			row++;
 		}
 		if ((col == n) & (h == 0))
@@ -245,7 +310,7 @@ Graph * * Load_Graph(char * Filename, int * size)
 		}
 	}
 	
-	return g;
+	return matrix;
 }
 
 
